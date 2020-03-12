66ec8d977741796f35e66877530f53ae1c58b502
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-03-12 18:09:59 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/jorgeferraz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jorgeferraz/workshopmongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.jorgeferraz.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jorgeferraz.workshopmongo.domain.User;
+import com.jorgeferraz.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping (value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping (method=RequestMethod.GET)
 	public ResponseEntity <List<User>> findAll(){
-		User jorge = new User("1", "jorge Ferraz", "jferraz@gamil.com");
-		User aline = new User("2", "Aline Ferraz", "alineferraz@gamil.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(jorge, aline));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
