a88c1236b728217e0ee4eef296140b94210ca95c
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-03-12 16:05:29 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/jorgeferraz/workshopmongo/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.jorgeferraz.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jorgeferraz.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping (value="/users")
+public class UserResource {
+
+	@RequestMapping (method=RequestMethod.GET)
+	public ResponseEntity <List<User>> findAll(){
+		User jorge = new User("1", "jorge Ferraz", "jferraz@gamil.com");
+		User aline = new User("2", "Aline Ferraz", "alineferraz@gamil.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(jorge, aline));
+		return ResponseEntity.ok().body(list);
+	}
+	
+
+}
