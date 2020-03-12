4ff176de0eef7c40de74178afb2812f99a80fd11
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-03-12 15:37:17 -0300

    Primeiro commit Projeto criado

---
+++ src/main/java/com/jorgeferraz/workshopmongo/ProjWorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeferraz.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjWorkshopmongoApplication {
+
+	public static void main(String[] args) {
+	 SpringApplication.run(ProjWorkshopmongoApplication.class, args);
+	}
+
+}
