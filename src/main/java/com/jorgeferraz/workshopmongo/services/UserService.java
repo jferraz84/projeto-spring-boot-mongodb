be2bc7126697234c8e01cdc9a3855cd43c9c04be
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-03-13 15:00:16 -0300

    Obtendo um usuário por id

--- src/main/java/com/jorgeferraz/workshopmongo/services/UserService.java
+++ src/main/java/com/jorgeferraz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jorgeferraz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jorgeferraz.workshopmongo.domain.User;
 import com.jorgeferraz.workshopmongo.repository.UserRepository;
+import com.jorgeferraz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,9 @@ public class UserService {
 		return repo.findAll();
 	}
 	
-	
+		public User findById(String id) {
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
 }
