481fad355f2b362565d79c0fed4e27d28619a1d8
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-04-07 19:16:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jorgeferraz/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.jorgeferraz.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jorgeferraz.workshopmongo.domain.Post;
+import com.jorgeferraz.workshopmongo.repository.PostRepository;
+import com.jorgeferraz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+}
