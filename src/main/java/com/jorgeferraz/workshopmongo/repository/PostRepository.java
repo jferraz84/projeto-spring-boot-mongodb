acf0496146add3349f9875ff0790d4a7fbeaa90b
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-04-10 15:06:11 -0300

    Consulta com vários critérios

--- src/main/java/com/jorgeferraz/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jorgeferraz/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.jorgeferraz.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -18,5 +19,12 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
+	@Query("{ $and: [ {date: {$gte: ?1} },{ date: { $lte: ?2} } ,"
+			+ " { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'body': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'coments.text': { $regex: ?0, $options: 'i' }  } ] } ] }")
+	
+	//@Query("{ $and: [ {date: {$gte: ?1} },{ date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch( String text, Date minDate, Date maxDate);
 	
 }
--- src/main/java/com/jorgeferraz/workshopmongo/services/PostService.java
+++ src/main/java/com/jorgeferraz/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.jorgeferraz.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,4 +29,8 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 
+	public List<Post> fullSearch( String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60* 60 *1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
